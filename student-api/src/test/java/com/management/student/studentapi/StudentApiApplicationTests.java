af6057e4fa15dd567463f040031067b1158f27a8
barthwalneha <nbarthwal30@gmail.com>
2023-08-25 13:08:33 +0530

    lets start

---
+++ student-api/src/test/java/com/management/student/studentapi/StudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.student.studentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
